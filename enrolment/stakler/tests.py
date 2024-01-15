5fe800772aef213e1846ecf0c517cd3a83a0fa0f
K. Thea <dev.fwslw@simplelogin.fr>
2024-01-15 17:01:38 +0100

    Create sample test

--- enrolment/stakler/tests.py
+++ enrolment/stakler/tests.py
@@ -1,1 +1,13 @@
 from django.test import TestCase
+from stakler.models import Teacher
+
+
+class TeacherTestCase(TestCase):
+    def setUp(self):
+        Teacher.objects.create(id=0, first_name='Tomasz', last_name='Testowy')
+
+
+    def test_creation(self):
+        t = Teacher.objects.get(id=0)
+        self.assertEqual(t.first_name, 'Tomasz')
+        self.assertEqual(t.last_name, 'Testowy')
